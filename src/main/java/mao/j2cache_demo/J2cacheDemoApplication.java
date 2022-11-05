9198fb3298d16450ec13d9385841a78418b41554
mao <1296193245@qq.com>
2022-11-05 13:02:21 +0800

    j2cache_demo

---
+++ src/main/java/mao/j2cache_demo/J2cacheDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.j2cache_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class J2cacheDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(J2cacheDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/j2cache_demo/J2cacheDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.j2cache_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J2cacheDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
